1ed504e36ac10f701709c9607d6bb3e2352cc265
vevedh <vevedh@gmail.com>
2024-09-30 14:57:45 -0400

    update

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,37 +2,67 @@
   <q-page class="fit row justify-center">
     <!-- content -->
 
-    <div class="column justify-center self-center q-pb-xl q-px-md" style="width: 400px">
-      <div class="row justify-center self-center rounded-borders q-pb-md" style="width: 200px">
+    <div
+      class="column justify-center self-center q-pb-xl q-px-md"
+      style="width: 400px"
+    >
+      <div
+        class="row justify-center self-center rounded-borders q-pb-md"
+        style="width: 200px"
+      >
         <q-img :src="url" spinner-color="white" class="rounded-borders" />
       </div>
-      <q-card class="my-card" bordered v-motion :initial="{
-        opacity: 0,
-        y: -100,
-      }" :enter="{
-        opacity: 1,
-        y: 0,
-        transition: {
-          type: 'spring',
-          stiffness: 350,
-          delay: 1500,
-
-        },
-
-      }" :duration="1500">
-        <q-card-section style="width: 100%" class="text-center bg-primary text-white text-h5 q-py-xs">
+      <q-card
+        class="my-card"
+        bordered
+        v-motion
+        :initial="{
+          opacity: 0,
+          y: -100,
+        }"
+        :enter="{
+          opacity: 1,
+          y: 0,
+          transition: {
+            type: 'spring',
+            stiffness: 350,
+            delay: 1500,
+          },
+        }"
+        :duration="1500"
+      >
+        <q-card-section
+          style="width: 100%"
+          class="text-center bg-primary text-white text-h5 q-py-xs"
+        >
           Identification
         </q-card-section>
         <q-separator />
         <q-card-section class="q-pb-none">
           <q-form class="q-px-xl">
-            <q-input filled bottom-slots v-model="email" label="*Utilisateur :" :dense="dense" lazy-rules>
+            <q-input
+              filled
+              bottom-slots
+              v-model="email"
+              label="*Utilisateur :"
+              :dense="dense"
+              lazy-rules
+            >
             </q-input>
-            <q-input filled bottom-slots type="password" v-model="password" label="Mot de passe :" :dense="dense">
+            <q-input
+              filled
+              bottom-slots
+              type="password"
+              v-model="password"
+              label="Mot de passe :"
+              :dense="dense"
+            >
             </q-input>
           </q-form>
         </q-card-section>
-        <q-card-actions class="row items-center justify-center q-mt-md q-pt-none">
+        <q-card-actions
+          class="row items-center justify-center q-mt-md q-pt-none"
+        >
           <div :hidden="!loading">
             <div class="col items-center justify-center">
               <div>
@@ -42,10 +72,18 @@
             </div>
           </div>
         </q-card-actions>
-        <q-card-actions class="row items-center justify-center q-mt-md q-pt-none">
+        <q-card-actions
+          class="row items-center justify-center q-mt-md q-pt-none"
+        >
           <div class="q-gutter-xl">
-            <q-btn color="primary" icon="lock" text-color="white" label="Connexion" @click="onSubmit(email, password)"
-              :disable="email == '' || password == ''" />
+            <q-btn
+              color="primary"
+              icon="lock"
+              text-color="white"
+              label="Connexion"
+              @click="onSubmit(email, password)"
+              :disable="email == '' || password == ''"
+            />
             <!--<q-btn color="primary" text-color="white" label="S'inscrire" @click="onRegister(email,password)" :disable="(email=='')&&(password=='')"/>-->
           </div>
         </q-card-actions>
@@ -62,14 +100,12 @@ const authStore = useAuthStore();
 const router = useRouter();
 const { api } = useFeathers();
 
-
-const variant = ref()
+const variant = ref();
 
 console.log('Api :', api);
 
 //import { mapGetters,mapState } from 'vuex'
 console.log('Auth login:', authStore);
-
 
 defineOptions({
   name: 'LonginPage',
@@ -77,7 +113,7 @@ defineOptions({
 
 const email = ref('');
 const password = ref('');
-const url = ref('cacem.png');
+const url = ref('fbavatar.png');
 const loading = ref(false);
 const dense = ref(true);
 //const showParticules = ref(false)
