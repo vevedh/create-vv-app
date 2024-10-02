712535f80c719f784e9391e1ca0e9f057be6be6f
vevedh <vevedh@gmail.com>
2024-10-02 06:53:37 -0400

    update

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -5,14 +5,6 @@ import { useAuth } from 'feathers-pinia';
 export const useAuthStore = defineStore('auth', () => {
   const { api } = useFeathers();
   const router = useRouter();
-  /*
-  const setup = async ({ user }) => {
-      await sleep(500)
-      // Make additional requests, populate data,
-      // return additional data, if desired.
-      return { user, foo: 'bar' }
-    }
-  */
 
   const auth = useAuth({ api, servicePath: 'users' });
   if (auth && !auth.isAuthenticated) {
@@ -21,23 +13,10 @@ export const useAuthStore = defineStore('auth', () => {
     auth.reAuthenticate();
   }
 
-  const isDSI = () => {
-    let result;
-    if (auth?.user) {
-      console.log('Auth :', auth.user.value);
-      result = Array.isArray(auth.user.value?.memberOf)
-        ? auth.user?.value.memberOf.find((o: any) =>
-            o.includes('Utilisateurs DSI'),
-          )?.length > 0
-        : false;
-    }
-    return result;
-  };
-
-  console.log('Auth useAuthStore:', { ...auth, isDSI: isDSI });
+  console.log('Auth useAuthStore:', { ...auth });
   return {
     ...auth,
-    isDSI: isDSI,
+
     logout: async () => {
       try {
         await auth.logout();
